9b13dbd685d967de8877a27719fe03b4f288c5da
JackieC1993 <jackiecheung@pursuit.org>
2023-06-21 22:14:34 -0400

    edit my main j.s

--- main.js
+++ main.js
@@ -1,47 +1,52 @@
-// Add a class of "featured" to the first section element
-const firstSection = document.querySelector('section');
-firstSection.classList.add('featured');
+let firstSection = document.querySelector("section");
+firstSection.classList.add("featured");
 
-// Create the new article element
-const newArticle = document.createElement('article');
-newArticle.innerHTML = `
-  <img src="./images/paul-gilmore-unsplash.jpg" alt="Image of a mountain in front of a lake." />
-  <h3>Stop Planning</h3>
-  <p>
-    You -- yes you! You're an over-planner, I can tell. It's time to stop
-    planning so much and instead focusing on relaxing. Taking a break at all is
-    so stressful these days; why add to the stress by overworking yourself?
-  </p>
-  <aside>
-    <p>
-      <strong>Read Time:</strong> 4 Minutes
-      <a href="#">Read more...</a>
-    </p>
-  </aside>
-`;
 
-// Add the new article to the end of the section.posts element
-const sectionPosts = document.querySelector('section.posts');
-sectionPosts.appendChild(newArticle);
 
-// Move the first article to become the second article
-const firstArticle = sectionPosts.querySelector('article');
-const secondArticle = sectionPosts.querySelector('article:nth-child(2)');
-sectionPosts.insertBefore(firstArticle, secondArticle);
+const newArticle = document.createElement("article");
 
-// Remove the "All Posts" link from the header
-const headerLink = document.querySelector('header nav ul li:first-child');
-headerLink.remove();
+const image = document.createElement("img");
+image.src = "./images/paul-gilmore-unsplash.jpg";
+image.alt = "Image of a mountain in front of a lake.";
+newArticle.appendChild(image);
 
-// Remove the span element that contains the "Read Time" in the featured post
-const featuredPost = document.querySelector('.featured');
-const readTimeSpan = featuredPost.querySelector('span');
-readTimeSpan.remove();
+const heading = document.createElement("h3");
+heading.textContent = "Stop Planning";
+newArticle.appendChild(heading);
 
-// Remove the last post from the page, titled "Stop Planning"
-const lastPost = document.querySelector('section.posts article:last-child');
-lastPost.remove();
+const paragraph = document.createElement("p");
+paragraph.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?";
+newArticle.appendChild(paragraph);
 
-// Remove all titles from all non-featured posts
-const nonFeaturedPosts = document.querySelectorAll('section.posts article:not(.featured) h3');
-nonFeaturedPosts.forEach((post) => post.remove());
+const aside = document.createElement("aside");
+const asideParagraph = document.createElement("p");
+const span = document.createElement("span");
+span.innerHTML = "<strong>Read Time:</strong> 4 Minutes";
+const readMoreLink = document.createElement("a");
+readMoreLink.href = "#";
+readMoreLink.textContent = "Read more...";
+asideParagraph.appendChild(span);
+asideParagraph.appendChild(readMoreLink);
+aside.appendChild(asideParagraph);
+newArticle.appendChild(aside);
+
+const sectionPosts = document.querySelector("section.posts");
+sectionPosts.appendChild(newArticle)
+
+
+
+const articles = sectionPosts.querySelectorAll("article");
+sectionPosts.insertBefore(articles[1], articles[0]);
+
+
+document.querySelectorAll("header a")[0].remove();
+
+
+document.querySelectorAll("span")[0].remove();
+
+
+newArticle.remove();
+
+
+const posts = document.querySelectorAll("section.posts h3");
+posts.forEach(post => post.remove())
