81e244aa80f51ecf5893a8a1cb93ecb18a0cc790
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-21 21:08:59 -0400

    Working on the last 5 problems

---
+++ main.js
@@ -0,0 +1,35 @@
+document.querySelector("section").classList.add("featured");
+
+let newElement = document.createElement("article");
+
+document.querySelector(".posts").appendChild(newElement);
+
+document.querySelectorAll("article")[3].setAttribute("class","thirdArticle");
+//created class attribute to access element 
+
+let image = document.createElement("img");
+
+image.setAttribute("src","./images/paul-gilmore-unsplash.jpg");
+
+document.querySelector(".thirdArticle").appendChild(image)
+
+let headerThree = document.createElement("h3");
+
+headerThree.innerText = "Stop Planning";
+
+document.querySelector(".thirdArticle").appendChild(headerThree)
+
+let paragraph = document.createElement("p")
+
+paragraph.innerText = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
+
+document.querySelector(".thirdArticle").append(paragraph);
+
+let aside = document.createElement("aside")
+
+aside.innerHTML = "<p> <span><strong>Read Time:</strong> 4 Minutes </span><a href='#'>Read more...</a>"
+
+document.querySelector(".thirdArticle").append(aside);
+
+document.querySelectorAll(".posts").article[1]
+
