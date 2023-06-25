21b3dffafda3e962b114f94511f880d4131b77b1
Natyka Callwood <Natykacallwood@pursuit.org>
2023-06-25 01:07:37 -0400

    Added a main.js file and linked it to the index.html file. Added a class of 'featured' to the first section element on the page. Created a another class of articleone on the first article. Going to see if adding classes to all the article elements will make it possible for me to distinguish between them easier so I can move the article around per the readME.md file instructions.

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstSection = document.querySelector("section");
+const featured = firstSection.classList.add("featured");
+
+// const newArticleElement = document.createElement("article")
+// newArticleElement.setAttribute()
+
+const firstArticle = document.querySelector("article")
+const articleOne = firstArticle.classList.add("articleone")
+
