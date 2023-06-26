e81bc17c222fb6e496e17cbfe8facf6431e8c8b1
Kenneth-Parker <kennethkavon@gmail.com>
2023-06-25 22:23:47 -0400

    Add a class of featured to the first  element on the page

---
+++ main.js
@@ -0,0 +1,4 @@
+
+// - [✓] Add a class of "featured" to the first `section` element on the page.
+const featuredSection = document.querySelector("section");
+featuredSection.classList.add("featured");
