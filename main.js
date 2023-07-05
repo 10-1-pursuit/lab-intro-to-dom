b9edefdfd356f0ec12afb6eb9bb4b0cbb3b72834
Natyka Callwood <Natykacallwood@pursuit.org>
2023-07-05 18:41:09 -0400

    Was able to add the first class of featured. Tried to add more classes to the other articles so I could change around the article orders but was not successful. When trying to remove the 'All posts' section from the header, removed the entire header. Still need to make some adjustments.

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const firstSection = document.querySelector("section");
 const featured = firstSection.classList.add("featured");
 
-// const newArticleElement = document.createElement("article")
-// newArticleElement.setAttribute()
+const newArticleElement = document.createElement("article")
+const newArticleElementWithClass = newArticleElement.classList.add("newarticle")
+const newPTagForArticleElementWithClass = document.createElement("p")
+newPTagForArticleElementWithClass.innerText = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focus on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
+const hThreeForNewArticleElement = document.createElement("h3")
+const hThreeForNewArticleElementWithClass = hThreeForNewArticleElement.classList.add("new-hthree")
+// hThreeForNewArticleElementWithClass.innerText = "Stop Planning"
+
+// const sectionPosts = document.querySelector(".posts")
+// const articleAddition = sectionPosts.append(hThreeForNewArticleElementWithClass, newPTagForArticleElementWithClass)
+// sectionPosts.remove(articleAddition)
 
 const firstArticle = document.querySelector("article")
 const articleOne = firstArticle.classList.add("articleone")
 
+const secondArticle = document.querySelector("article")
+const articleTwo = secondArticle.classList.add("articletwo")
